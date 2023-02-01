ce8cfa610d5fb8b9a8eb010c28cc4fafb2c37653
y4nci <baran.yanci@metu.edu.tr>
2023-02-01 03:02:18 +0300

    change font weight & refactor code

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -6,12 +6,7 @@ import { areInputsValid } from '../lib/validation';
 import { InputService } from '../services/inputService';
 import { WarningService } from '../services/warningService';
 
-/**
- * The component that contains the button that calculates the fee and the fee itself.
- */
-export const Calculator = () => {
-    const [fee, setFee] = useState<Fee>(null);
-
+const CalculateButton = (props) => {
     const calculate = () => {
         const { cart, distance, items, orderTime } = InputService.getInstance().getInputs();
         const totalFee = calculateFee(cart, distance, items, orderTime);
@@ -19,24 +14,41 @@ export const Calculator = () => {
     };
 
     return (
-        <div className="calculator flexCenterColumn">
-            <Button style={{ fontSize: 'large' }} intent="primary"
-                onClick={() => {
-                    const [isValid, invalids] = areInputsValid();
+        <Button style={{ fontSize: 'large' }} intent="primary"
+            onClick={() => {
+                const [isValid, invalids] = areInputsValid();
 
-                    if (!isValid) {
-                        new WarningService().warn(invalids);
-                        return;
-                    }
-                    setFee(calculate());
-                }}
-                icon="calculator"
-            >Calculate</Button>
+                if (!isValid) {
+                    new WarningService().warn(invalids);
+                    return;
+                }
+                props.setFee(calculate());
+            }}
+            icon="calculator"
+        >Calculate</Button>
+    );
+};
+
+const FeeView = (props) => {
+    return (
+        <div className="fee">
+            <p>In total:&nbsp;</p>
+            <p className="feeAmount">{props.fee}€</p>
+        </div>
+    );
+};
+
+/**
+ * The component that contains the button that calculates the fee and the fee itself.
+ */
+export const Calculator = () => {
+    const [fee, setFee] = useState<Fee>(null);
+
+    return (
+        <div className="calculator flexCenterColumn">
+            <CalculateButton setFee={setFee} />
             {fee !== null &&
-                <div className="fee">
-                    <p>In total:&nbsp;</p>
-                    <p className="feeAmount">{fee}€</p>
-                </div>
+                <FeeView fee={fee} />
             }
         </div>
     );
